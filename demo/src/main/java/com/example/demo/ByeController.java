6338cf250b088a4be34097b86c3294db68b20727
李怀东 <lihuaidong@sf-express.com>
2018-11-19 17:41:47 +0800

    增加kafka demo

---
+++ demo/src/main/java/com/example/demo/ByeController.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+/**
+ * @Description
+ * @Author: Lihuaidong
+ * @Date: Created at 18:01 2018/11/10
+ */
+public class ByeController {
+}
---
+++ kafka/src/main/java/hdli/ConsumerMain.java
@@ -0,0 +1,9 @@
+package hdli;
+
+/**
+ * @Description
+ * @Author: Lihuaidong
+ * @Date: Created at 17:56 2018/11/13
+ */
+public class ConsumerMain {
+}
