b07195fe89ceb4f35c5ea468a561f1bec8f5d842
lihuaidong <hdli168@163.com>
2018-11-28 21:54:30 +0800

    增加插入排序

--- sort/src/main/java/hdli/InsertionSort.java
+++ sort/src/main/java/hdli/InsertionSort.java
@@ -8,13 +8,32 @@ package hdli;
 public class InsertionSort {
 
 	public static void main(String[] args) {
-		int[] array = new int[] { 12, 23, 33, 11, 2, 11, 3, 12, 3, 12, 11, 33, 44, 1 };
+		int[] array = new int[]{12, 23, 33, 11, 2, 11, 3, 12, 3, 12, 11, 33, 44, 1};
 		int length = array.length;
-		int[] temp = new int[length];
-		temp[0] = array[0];
-		for(int i=0; i<length; i++) {
+		int tmp = 0;
+		for (int i = 1; i < length; i++) {
+			tmp = array[i];
+			int j = i;
 
+//			for (; j > 0; j--) {
+//				if(tmp < array[j-1]) {
+//					array[j] = array[j-1];
+//				} else {
+//					break;
+//				}
+//			}
+//			array[j] = tmp;
+
+
+			while(j>0 && tmp < array[j-1]) {
+				array[j] = array[--j];
+			}
+			array[j] = tmp;
+
+			for(int data : array) {
+				System.out.print(data + " ");
+			}
+			System.out.println();
 		}
 	}
-
 }
