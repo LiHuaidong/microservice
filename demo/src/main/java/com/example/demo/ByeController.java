b1a6c768b3407402b48b03559bde74bfdeb78b3c
lihuaidong <hdli168@163.com>
2019-01-25 15:38:29 +0800

    增加注解

--- demo/src/main/java/com/example/demo/ByeController.java
+++ demo/src/main/java/com/example/demo/ByeController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,10 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date: Created at 18:01 2018/11/10
  */
 @Controller
-@RequestMapping("/bye")
+@RequestMapping("bye")
 public class ByeController {
 
 	@RequestMapping("/print1")
+	@ResponseBody
 	public String index() {
 		return "Bye-World";
 	}
