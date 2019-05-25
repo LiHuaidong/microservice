f85ee72af66d224b06797499595813ef61a668c5
lihuaidong <hdli168@163.com>
2019-05-25 19:37:31 +0800

    增加spring cloud stream

---
+++ stream-provider/src/test/java/hdli/streamprovider/StreamProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package hdli.streamprovider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class) @SpringBootTest public class StreamProviderApplicationTests {
+
+	@Test public void contextLoads() {
+	}
+
+}
