ec1ebb21500f7b05e95ca65923c1bcd6ad3e5ce8
lihuaidong <hdli168@163.com>
2019-05-29 00:22:45 +0800

    增加aop-demo

---
+++ aop-demo/src/main/java/hdli/aopdemo/AopDemoApplication.java
@@ -0,0 +1,13 @@
+package hdli.aopdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AopDemoApplication.class, args);
+    }
+
+}
