511e565134a4b3e79a6aed06ee99ae1e9988b7c3
lihuaidong <hdli168@163.com>
2019-05-30 00:23:10 +0800

    增加aop-demo

--- aop-demo/src/main/java/hdli/aopdemo/AspectjLog.java
+++ aop-demo/src/main/java/hdli/aopdemo/AspectjLog.java
@@ -1,9 +1,8 @@
 package hdli.aopdemo;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
@@ -16,14 +15,43 @@ public class AspectjLog {
 
     private Logger logger = LoggerFactory.getLogger(AspectjLog.class);
 
-    @Pointcut("execution(* hdli.service.AspectService.*(..))")
-    public void logAop(){}
+    @Pointcut("execution(* hdli.service.impl.AspectService.*(..))")
+    public void logAop(){
+        logger.debug("logAop {}", 123);
+    }
 
+    @Before("logAop() && args(email)")
     public void logBefore(JoinPoint joinPoint, String email) {
         logger.debug("前置通知Before>>{}", email);
     }
 
     @AfterReturning("logAop() && args(email)")
-    public void logAfterReturning(String email) {}
+    public void logAfterReturning(String email) {
+        logger.debug("返回通知AfterReturning>>{}", email);
+    }
+
+    @After("logAop() && args(email)")
+    public void logAfter(String email) {
+        logger.info("后置通知After>>{}", email);
+    }
+
+    @AfterThrowing("logAop() && args(email)")
+    public void logAfterThrow(String email) {
+        logger.info("异常通知AfterThrowing>>{}", email);
+    }
+
+    //环绕通知功能很强,可以替代上面的所有通知
+    @Around("logAop() && args(email)")
+    public void logAround(ProceedingJoinPoint jp, String email) {
+        try {
+            logger.info("自定义前置通知Before>>>{}", email);
+            jp.proceed();//将控制权交给被通知的方法，也就是执行sayHello方法
+            logger.info("自定义返回通知AfterReturning>>>{}", email);
+        } catch (Throwable throwable) {
+            logger.info("异常处理>>>>{}", email);
+            throwable.printStackTrace();
+        }
+        logger.info("自定义后置通知After>>>{}", email);
+    }
 
 }
--- aop-demo/src/main/java/hdli/service/AspectService.java
+++
@@ -1,4 +0,0 @@
-package hdli.service;
-
-public class AspectService {
-}
