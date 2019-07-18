e85e1a6f1fcb3a00243094f77fa5cbcb02df9178
lihuaidong <hdli168@163.com>
2019-07-18 23:42:19 +0800

    增加queue

--- thread-demo/src/main/java/hdli/transferqueue/Consumer.java
+++ thread-demo/src/main/java/hdli/transferqueue/Consumer.java
@@ -2,7 +2,7 @@ package hdli.transferqueue;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.concurrent.TransferQueue;
+import java.util.concurrent.LinkedTransferQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -11,14 +11,13 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @Date: Created at 10:27 2019/7/15
  */
 public class Consumer implements Runnable {
-	private static final Logger logger = LoggerFactory.getLogger(Consumer.class);
 
-	private final TransferQueue<String> transferQueue;
+	private final LinkedTransferQueue<String> transferQueue;
 	private final String name;
 	final int numberOfMessagesToConsume;
 	final AtomicInteger numberOfConsumedMessages = new AtomicInteger();
 
-	Consumer(TransferQueue<String> transferQueue, String name, int numberOfMessagesToConsume) {
+	Consumer(LinkedTransferQueue<String> transferQueue, String name, int numberOfMessagesToConsume) {
 		this.transferQueue = transferQueue;
 		this.name = name;
 		this.numberOfMessagesToConsume = numberOfMessagesToConsume;
@@ -27,10 +26,10 @@ public class Consumer implements Runnable {
 	public void run() {
 		for (int i = 0; i < numberOfMessagesToConsume; i++) {
 			try {
-				logger.debug("Consumer: " + name + " is waiting to take element...");
+				System.out.println("Consumer: " + name + " is waiting to take element...");
 				String element = transferQueue.take();
 				longProcessing(element);
-				logger.debug("Consumer: " + name + " received element: " + element);
+				System.out.println("Consumer: " + name + " received element: " + element);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
--- thread-demo/src/main/java/hdli/transferqueue/Producer.java
+++ thread-demo/src/main/java/hdli/transferqueue/Producer.java
@@ -4,8 +4,8 @@ package hdli.transferqueue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.LinkedTransferQueue;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TransferQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -15,14 +15,12 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class Producer implements Runnable {
 
-	private static final Logger logger = LoggerFactory.getLogger(Producer.class);
-
-	private final TransferQueue<String> transferQueue;
+	private final LinkedTransferQueue<String> transferQueue;
 	private final String name;
 	final int numberOfMessagesToProduce;
 	final AtomicInteger numberOfProducedMessages = new AtomicInteger();
 
-	Producer(TransferQueue<String> transferQueue, String name, Integer numberOfMessagesToProduce) {
+	Producer(LinkedTransferQueue<String> transferQueue, String name, Integer numberOfMessagesToProduce) {
 		this.transferQueue = transferQueue;
 		this.name = name;
 		this.numberOfMessagesToProduce = numberOfMessagesToProduce;
@@ -31,18 +29,14 @@ public class Producer implements Runnable {
 	public void run() {
 		try {
 			for (int i = 0; i < numberOfMessagesToProduce; i++) {
-				if(logger.isDebugEnabled()) {
-					logger.debug("Producer: " + name + " is waiting to transfer...");
-				}
+				System.out.println("Producer: " + name + " is waiting to transfer...");
 
 				boolean added = transferQueue.tryTransfer("A" + i, 4000, TimeUnit.MILLISECONDS);
 				if(added) {
 					numberOfProducedMessages.incrementAndGet();
-					if (logger.isDebugEnabled()) {
-						logger.debug("Producer: " + name + " transferred element: A" + i);
-					}
+					System.out.println("Producer: " + name + " transferred element: A" + i);
 				} else {
-					logger.debug("can not add an element due to the timeout");
+					System.out.println("can not add an element due to the timeout");
 				}
 			}
 		} catch (InterruptedException e) {
