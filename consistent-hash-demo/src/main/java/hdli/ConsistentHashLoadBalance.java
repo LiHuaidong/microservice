69e7fa276a0cf10703b5f823a2ff507928832e85
李怀东 <lihuaidong@sf-express.com>
2019-09-20 11:15:54 +0800

    update

--- consistent-hash-demo/src/main/java/hdli/ConsistentHashLoadBalance.java
+++ consistent-hash-demo/src/main/java/hdli/ConsistentHashLoadBalance.java
@@ -4,6 +4,7 @@ import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.TreeMap;
 
 public class ConsistentHashLoadBalance {
@@ -19,10 +20,10 @@ public class ConsistentHashLoadBalance {
     }
 
     private void initalization() {
-        for(String nodeName : nodes) {
-            for(int i=0; i<replicCnt/4; i++) {
+        for (String nodeName : nodes) {
+            for (int i = 0; i < replicCnt / 4; i++) {
                 String virtualNodeName = getNodeNameByIndex(nodeName, i);
-                for(int j=0; j<4; j++) {
+                for (int j = 0; j < 4; j++) {
                     virtualNodes.put(hash(virtualNodeName, j), nodeName);
                 }
             }
@@ -33,9 +34,27 @@ public class ConsistentHashLoadBalance {
         return new StringBuffer(nodeName).append("&&").append(index).toString();
     }
 
+    public String selectNode(String key) {
+        Long hashOfKey = hash(key, 0);
+        if (!virtualNodes.containsKey(hashOfKey)) {
+            Map.Entry<Long, String> entry = virtualNodes.ceilingEntry(hashOfKey);
+            if (entry != null) {
+                return entry.getValue();
+            } else {
+                return nodes.getFirst();
+            }
+        } else {
+            return virtualNodes.get(hashOfKey);
+        }
+    }
+
     private Long hash(String nodeName, int number) {
         byte[] digest = md5(nodeName);
-        return null;
+
+        return (((long) (digest[3 + number * 4] & 0xFF) << 24)
+                | ((long) (digest[2 + number * 4] & 0xFF) << 16)
+                | ((long) (digest[1 + number * 4] & 0xFF) << 8)
+                | (digest[number * 4] & 0xFF)) & 0xFFFFFFFFL;
     }
 
     public byte[] md5(String str) {
@@ -51,5 +70,43 @@ public class ConsistentHashLoadBalance {
             e.printStackTrace();
             return null;
         }
+    }
+
+    public void addNode(String node) {
+        nodes.add(node);
+        String virtualNodeName = getNodeNameByIndex(node, 0);
+        for (int i = 0; i < replicCnt / 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                virtualNodes.put(hash(virtualNodeName, j), node);
+            }
+        }
+    }
+
+    public void removeNode(String node) {
+        nodes.remove(node);
+        String virtualNodeName = getNodeNameByIndex(node, 0);
+        for (int i = 0; i < replicCnt / 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                virtualNodes.remove(hash(virtualNodeName, j), node);
+            }
+        }
+    }
+
+    private void printTreeNode() {
+        if (virtualNodes != null && !virtualNodes.isEmpty()) {
+            virtualNodes.forEach((hashKey, node) -> System.out.println(new StringBuffer(node).append(" ==> ").append(hashKey)));
+        } else {
+            System.out.println("Cycle is Empty");
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList<String> nodes = new LinkedList<>();
+        nodes.add("192.168.2.1:8080");
+        nodes.add("192.168.2.2:8080");
+        nodes.add("192.168.2.3:8080");
+        nodes.add("192.168.2.4:8080");
+        ConsistentHashLoadBalance consistentHash = new ConsistentHashLoadBalance(nodes, 160);
+        consistentHash.printTreeNode();
     }
 }
